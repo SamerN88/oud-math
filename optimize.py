8e52c8354325bf4220e1f0c7ecd71c22fccfe4f8
Samer N. Najjar <s.najjar612@gmail.com>
2024-10-29 12:21:32 -0500

    Implement L-BFGS-B in Optimizer for fitting a model to points

--- optimize.py
+++ optimize.py
@@ -1,284 +1,122 @@
-import itertools
-import textwrap
-
 import numpy as np
-from matplotlib import pyplot as plt
+from scipy.optimize import minimize
 
-from models import GeneralizedGDModel, GrowthDecayModel, StaticModel, BezierModel
-from util import Stopwatch, points_from_file
+from models import GeneralizedGDModel, BezierModel
+from util import Style
 
 
 class Optimizer:
-    def __init__(self, model, points):
+    def __init__(self, model, *, param_bounds, init_guess=None, verbose=False):
         self.model = model
-        self.points = np.array(points, copy=True)
 
-        # TODO: must replace this implementation with L-BFGS-B!!!
+        self.param_bounds = param_bounds
+        if init_guess is None:
+            init_guess = [np.mean(bounds) for bounds in param_bounds]
+        self.init_guess = np.array(init_guess)
 
-        CONFIG_GETTERS = {
-            GeneralizedGDModel: self._get_config__GeneralizedGDModel,
-            BezierModel: self._get_config__BezierModel,
-        }
+        self.verbose = bool(verbose)
+        self.print = print if verbose else (lambda *_, **__: None)
 
-        self.get_config = CONFIG_GETTERS[type(model)]
+        self.opt_params = None
 
-    def run(self, density, epochs=1, *, animate=False):
-        if epochs == 1:
-            best_params, best_mae = self.brute_force_fit(density, animate=animate)
-        else:
-            best_params, best_mae = self.convergent_brute_force_fit(density, epochs, animate=animate)
-        self._print_results(best_params, best_mae)
-        plt.show()
+    def fit(self, points):
+        points = np.array(points, copy=True)
 
-    def brute_force_fit(self, density, *, animate=False, ax=None, param_space_dict=None):
-        model = self.model
-        points = self.points
-        if param_space_dict is None:
-            param_space_dict, _ = self.get_config(density)
+        # The function we want to minimize; given params, we set them as model params then compute and return the MSE
+        def objective_function(params):
+            self._set_model_params(params)
+            return self.mse(points)
 
-        print(f'Optimizing model:  {model}')
-        print(f'Number of points to fit: {len(points):,}')
+        # Run L-BFGS-B to find parameters that optimally fit the model to the given points
+        result = minimize(objective_function, self.init_guess, method='L-BFGS-B', bounds=self.param_bounds)
+        self.opt_params = result.x
 
-        x = points[:, 0]
-        y = points[:, 1]
+        # Ensure model is configured with optimal parameters
+        self._set_model_params(self.opt_params)
 
-        # Initialize params and MAE
-        best_params = model.get_params()
-        best_mae = self.mae(y, model(x))
+        # Performance metrics
+        mse = self.mse(points)
+        mae = self.mae(points)
+        R2 = self.R2(points)
 
-        # Setup logging
-        total_iters = np.prod([len(space) for space in param_space_dict.values()])
-        checkpoint = step_size = 10  # percent completion per log message
+        # [Verbose] Show optimal parameters, optimized model, performance metrics, and plot model curve against points
+        col_width = max(map(len, self.model.PARAM_NAMES))
+        self.print()
+        self.print('-' * 75)
+        self.print(Style('OPTIMIZER RESULTS').bold())
+        self.print()
+        self.print(Style('Optimal Parameters:').blue())
+        self.print(*(f'{name.ljust(col_width)} = {value}' for name, value in self.model.params.items()), sep='\n')
+        self.print()
+        self.print(Style('Optimized Model:').blue())
+        self.print(self.model)
+        self.print()
+        self.print(Style('Performance Metrics:').green())
+        self.print(f'MSE:  {mse}')
+        self.print(f'MAE:  {mae}')
+        self.print(f'R^2:  {R2}')
+        self.print('-' * 75)
+        self.print()
+        if self.verbose:
+            self.model.plot(points=points)
 
-        # Get parameter space generator
-        param_space = self.generate_param_space(param_space_dict)
+        return self.model
 
-        # Initialize animation if enabled
-        if animate:
-            plt.ion()
-            if ax is None:
-                _fig, ax = plt.subplots()
-            plot_height = max(points[:, 1]) * 1.5
+    def mse(self, points):
+        x, y = points.T
+        y_pred = self.model(x)
+        return np.mean((y - y_pred) ** 2)
 
-            def update_animation():
-                ax.cla()  # clear the current axes
-                wrapped_model_repr = textwrap.fill(str(model), width=60)
-                model.plot(points=points, ax=ax, show=False, title=f'{wrapped_model_repr}\n\nMAE: {best_mae:.6f} mm')
-                ax.title.set_fontsize(10)
-                ax.set_ylim(0, plot_height)
-                plt.pause(0.001)  # pause to allow the plot to update
+    def mae(self, points):
+        x, y = points.T
+        y_pred = self.model(x)
+        return np.mean(np.abs(y - y_pred))
 
-            update_animation()
+    def R2(self, points):
+        x, y = points.T
+        y_pred = self.model(x)
+        residual_sum_of_squares = np.sum((y - y_pred) ** 2)
+        total_sum_of_squares = np.sum((y - np.mean(y)) ** 2)
+        return 1 - (residual_sum_of_squares / total_sum_of_squares)
 
-        # Iterate over parameter space and return optimal parameters
-        print(f'Total iterations: {total_iters:,}')
-        stopwatch = Stopwatch()
-        stopwatch.start()
-        for i, params in enumerate(param_space, 1):
-            model.set_params(**params)
-
-            current_mae = self.mae(y, model(x))
-            if current_mae < best_mae:
-                best_mae = current_mae
-                best_params = params
-
-                # Update animation if enabled
-                if animate:
-                    update_animation()
-
-            if i % 10000 == 0 or i == total_iters:
-                print(f'    {i:,} iters done [{round(i / total_iters * 100)}%] [{stopwatch.lap():.3f} s]  :  MAE={best_mae}, params={best_params}')
-
-        model.set_params(**best_params)
-
-        if animate:
-            ax.title.set_weight('bold')
-            plt.ioff()
-
-        return best_params, best_mae
-
-    def convergent_brute_force_fit(self, density, epochs, *, animate=False):
-        model = self.model
-        points = self.points
-        param_space_dict, param_space_schedules = self.get_config(density)
-
-        # Just to get initial baseline MAE
-        x = points[:, 0]
-        y = points[:, 1]
-
-        # Initialize
-        best_params = model.get_params()
-        best_mae = self.mae(y, model(x))
-
-        total_iters = np.prod([len(space) for space in param_space_dict.values()])
-        print('* ' * 21)
-        print('CONVERGENT BRUTE FORCE FIT'.center(40))
-        print('* ' * 21)
-        print()
-        print(f'Number of epochs: {epochs:,}')
-        print(f'Total iterations across all epochs: {total_iters * epochs:,}')
-        print()
-
-        if animate:
-            _fig, ax = plt.subplots()
-        else:
-            ax = None
-
-        stopwatch = Stopwatch()
-        stopwatch.start()
-        for epoch in range(1, epochs + 1):
-            print('*' * 20 + f' EPOCH {epoch} ' + '*' * 20)
-            best_params, best_mae = self.brute_force_fit(density, param_space_dict=param_space_dict, animate=animate, ax=ax)
-            print()
-
-            if epoch == epochs:
-                break
-
-            for name, value in best_params.items():
-                current_space = param_space_dict[name]
-                param_space_dict[name] = param_space_schedules[name](value, current_space)
-
-        print(f'Total runtime: {stopwatch.total_elapsed():.3f} s')
-
-        return best_params, best_mae
-
-    @staticmethod
-    def generate_param_space(param_space_dict):
-        names = param_space_dict.keys()
-        spaces = param_space_dict.values()
-        combinations = itertools.product(*spaces)
-
-        for combination in combinations:
-            yield dict(zip(names, combination))
-
-    @staticmethod
-    def create_schedule(lower, upper, density):
-        def refine_param_space(param_value, space):
-            space_width = max(space) - min(space)
-            margin = 1 / (density - 1)  # if density=N, we have N points and N-1 intervals, so each interval is 1/(N-1) of the space width
-            refined_space = np.linspace(
-                max(lower, param_value - margin * space_width),
-                min(upper, param_value + margin * space_width),
-                density
-            )
-            return np.unique(refined_space)
-
-        return refine_param_space
-
-    @staticmethod
-    def mae(y_true, y_pred):
-        return np.mean(np.abs(y_true - y_pred))
-
-    def _print_results(self, best_params, best_mae):
-        model = self.model
-        col_width = max(map(len, model.PARAM_NAMES))
-        print()
-        print('_' * 75)
-        print('RESULTS')
-        print()
-        print(*(f'{name.ljust(col_width)} = {value}' for name, value in best_params.items()), sep='\n')
-        print()
-        print(model)
-        print('MAE:', best_mae, 'mm')
-
-    # Config methods ---------------------------------------------------------------------------------------------------
-
-    def _get_config__GeneralizedGDModel(self, density):
-        # Define complete param space
-        param_space_dict = {
-            'alpha': np.linspace(0, 1, density),
-            'beta': np.linspace(0, 1, density),
-            'k': np.linspace(0, 2, density)
-        }
-
-        # Define schedules for narrowing param space
-        schedules = {
-            'alpha': self.create_schedule(0, 1, density=density),
-            'beta': self.create_schedule(0, 1, density=density),
-            'k': self.create_schedule(0, 2, density=density)
-        }
-
-        return param_space_dict, schedules
-
-    def _get_config__BezierModel(self, density):
-        unit_space = np.linspace(0, 1, density)
-        num_inner_control_points = self.model.degree - 1
-
-        # Create complete param space
-        param_space_dict = {'x1': [0], 'y1': unit_space}
-        x_marks = np.linspace(0, 1, num_inner_control_points)
-        for i in range(2, num_inner_control_points + 1):
-            x_key = f'x{i}'
-            y_key = f'y{i}'
-            start_x_mark = x_marks[i - 2]
-            end_x_mark = x_marks[i - 1]
-            param_space_dict[x_key] = np.linspace(start_x_mark, end_x_mark, density)
-            param_space_dict[y_key] = unit_space
-
-        # Create schedules for narrowing param space
-        schedules = {}
-        for i in range(1, num_inner_control_points + 1):
-            x_key = f'x{i}'
-            y_key = f'y{i}'
-            schedules[x_key] = self.create_schedule(0, 1, density=density)
-            schedules[y_key] = self.create_schedule(0, 1, density=density)
-
-        return param_space_dict, schedules
+    def _set_model_params(self, params):
+        self.model.set_params(**{name: value for name, value in zip(self.model.PARAM_NAMES, params)})
 
 
 def main():
-    # Define target points
-    # target_model = BezierModel(H=500, Z=60, x1=0.0, y1=0.6252800000000002, x2=0.5055999999999999, y2=0.33664000000000005, x3=0.6624, y3=0.41472)
-    # x = np.linspace(0, 500, 1000)
-    # y = target_model(x)
-    # points = np.column_stack((x, y))
-    points = points_from_file('SANDBOX/points.txt')
+    # Generate points using GeneralizedGDModel, then fit a BezierModel to them
 
-    # Scale points as desired (the neck thickness will be warped, but we can still get a good model fit since the model
-    # itself is mathematically fixed at (H, Z/2)
-    # points = scale(points, x_max=500, y_max=185)
+    def noise(delta, size):
+        return np.random.uniform(-delta, delta, size)
 
-    #################
-    USE_BEZIER = True
-    #################
+    # Generate data points we want to fit to
+    target_model = GeneralizedGDModel(H=500, Z=60, alpha=0.4, beta=0.9, k=0.75)
+    x = np.linspace(0, target_model.H, 100)
+    y = target_model(x) + noise(5, len(x))
+    points = np.array(list(zip(x, y)))
 
-    # Choose param space density (per parameter) and num epochs
-    if USE_BEZIER:
-        density = 20
-        epochs = 4
-        num_inner_control_points = 3
-        model = BezierModel([(i, 1) for i in np.linspace(0, 1, num_inner_control_points)], H=500, Z=60)
-    else:
-        density = 40
-        epochs = 5
-        model = GeneralizedGDModel(H=500, Z=60, alpha=0.5, beta=1, k=1)
+    # Instantiate BezierModel that we want to optimize
+    degree = 3
+    inner_control_points = [(i, 0) for i in np.linspace(0, 1, degree)]
+    model = BezierModel(inner_control_points, H=500, Z=60)
 
-    # Create optimizer and fit the model to the points
-    optimizer = Optimizer(model, points)
-    optimizer.run(density, epochs, animate=False)
+    # Define param bounds for the BezierModel (must do this carefully as x must be strictly increasing for CubicSpline)
+    left_skew = 3  # generally, we get better accuracy with denser control points near x=0 (steepest part of bowl)
+    x_bounds = [((i/degree)**left_skew, ((i+1)/degree)**left_skew) for i in range(degree)]
+    y_bounds = [(0, 1)] * degree
+    param_bounds = [val for pair in zip(x_bounds, y_bounds) for val in pair]  # interleave x and y bounds
+
+    # See model curve before fitting
+    model.plot(points=points)
+
+    # Instantiate optimizer and fit it to our generated data points
+    optimizer = Optimizer(
+        model,
+        param_bounds=param_bounds,
+        verbose=True
+    )
+    optimizer.fit(points)
 
 
 if __name__ == '__main__':
     main()
-
-'''
-Optimized models (on points from file):
-
-
-y_max = 200 ------------------------------------------------------------------------------------------------------------
-
-BezierModel(H=500, Z=60, x1=0.0, y1=0.6521600000000001, x2=0.5584, y2=0.3209600000000001, x3=0.6823999999999999, y3=0.39392)
-MAE: 0.5851086024146657 mm
-
-GeneralizedGDModel(H=500, Z=60, alpha=0.44711230149622744, beta=0.9318595905100431, k=1.0008791730520934)
-MAE: 1.598603000947343 mm
-
-y_max = 185 ------------------------------------------------------------------------------------------------------------
-
-BezierModel(H=500, Z=60, x1=0.0, y1=0.375, x2=0.1875, y2=0.390625, x3=0.53125, y3=0.484375)
-MAE: 1.3052330710672473 mm
-
-GeneralizedGDModel(H=500, Z=60, alpha=0.4406588081762854, beta=0.9506194452333127, k=0.932957842052995)
-MAE: 1.4683382513168537 mm
-
-'''
